685fe4d85b9196ebc268621363e3cadd52c5f382
Leopardeco <leeonardoolliveira1@gmail.com>
2023-09-26 22:01:29 -0300

    exercicio2

--- index.sql
+++ index.sql
@@ -8,3 +8,18 @@ end;
 delimiter ;
 call sp_ListarAutores();
 drop procedure sp_ListarAutores;
+
+-- Exercicio 2 
+DELIMITER //
+create procedure sp_LivrosPorCategoria (in categoria_nome varchar(255))
+begin
+    select Livro.Titulo, Livro.Editora_ID, Livro.Ano_Publicacao, Livro.Numero_Paginas
+    from Livro
+    inner join Categoria on Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+end;
+//
+DELIMITER ;
+call sp_LivrosPorCategoria('Romance');
+call sp_LivrosPorCategoria('Ciência');
+drop procedure sp_LivrosPorCategoria;
