3b7d41add68de289b456292e93feeae3ff71bd29
Leopardeco <leeonardoolliveira1@gmail.com>
2023-09-26 22:04:06 -0300

    exercicio4

--- index.sql
+++ index.sql
@@ -35,3 +35,31 @@ begin
 end;
 //
 delimiter ;
+
+CALL sp_ContarLivrosPorCategoria('História', @total);
+select @total;
+drop procedure sp_ContarLivrosPorCategoria;
+
+-- Exercicio 4
+delimiter //
+create procedure sp_VerificarLivrosCategoria(in categoria_nome varchar(255), out possui_livros bit)
+begin
+    declare total_livros int;
+    
+    select count(*) into total_livros
+    from Livro
+    inner join Categoria on Livro.Categoria_ID = Categoria.Categoria_ID
+    where Categoria.Nome = categoria_nome;
+    
+    if total_livros > 0 then
+        set possui_livros = 1;
+    else
+        set possui_livros = 0;
+    end if;
+end;
+//
+delimiter ;
+
+call sp_VerificarLivrosCategoria('História', @possui_livros);
+select @possui_livros;
+drop procedure sp_VerificarLivrosCategoria;
